685131371a7421e680f71dd0ee977399d0edf9ba
SebastianDevus <sebastiandevus@gmail.com>
2022-09-08 22:24:12 -0300

    Adiciona três cálculos de Bhaskara. Testar cálculos de raízes

--- Calculadora.Lib/CalculosDiversos.cs
+++ Calculadora.Lib/CalculosDiversos.cs
@@ -1,6 +1,19 @@
 namespace Calculadora.Lib;
 
-public class CalculosMiscelanios
+public class CalculosDiversos
 {
-    
+    public static double BhaskaraCalculaDelta(double a, double b, double c)
+    {
+        return Math.Pow(b, 2) - 4 * a * c;
+    }
+
+    public static double BhaskaraCalculaRaiz1(double a, double b, double delta)
+    {
+        return (-b + Math.Sqrt(delta)) / (2 * a);
+    }
+
+    public static double BhaskaraCalculaRaiz2(double a, double b, double delta)
+    {
+        return (-b - Math.Sqrt(delta)) / (2 * a);
+    }
 }
--- Calculadora.Tests/CalculosDiversosTest.cs
+++ Calculadora.Tests/CalculosDiversosTest.cs
@@ -3,3 +3,18 @@ using Calculadora.Lib;
 
 namespace Calculadora.Tests;
 
+public class CalculosDiversosTest
+{
+    [Theory]
+    [InlineData(1, 2, 3, -8)]
+    [InlineData(3, 2, 1, -8)]
+    [InlineData(2, 2, 2, -12)]
+    [InlineData(5, 3, 50, -991)]
+    [InlineData(1, 2, -3, 16)]
+    public void BhaskaraCalculaDeltaCalculaCorretamente(double a, double b, double c, double resultadoEsperado)
+    {
+        Assert.Equal(resultadoEsperado, CalculosDiversos.BhaskaraCalculaDelta(a, b, c));
+    }
+
+    
+}
